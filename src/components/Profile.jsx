42c30c082f09a368913b1286f3cccaed8221b441
Pavel <bm38kpk@gmail.com>
2022-03-12 17:18:13 +0800

    release v1

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+    <div>
+      <img src='https://srv4.imgonline.com.ua/result_img/imgonline-com-ua-Resize-dYqcJNaTEUf8.jpg' />
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
