f4feb0fb14136272db5df29f1e8262ffbced0620
Pavel <bm38kpk@gmail.com>
2022-03-31 15:33:42 +0800

    Class Component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,57 +1,49 @@
 import React from "react";
+import * as axios from "axios";
 import styles from './Users.module.css';
+import userPhoto from '../../assets/images/user.png';
 
-const Users = (props) => {
+class Users extends React.Component {
 
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1, photoURL: 'https://icdn.lenta.ru/images/2021/07/21/20/20210721200926212/square_320_3b7d8e82f70387b4bc934cb3da865b8f.jpg',
-                followed: true, fullName: 'Alex', status: 'I am here now', location: { city: 'Moscow', country: 'Russia' }
-            },
-            {
-                id: 2, photoURL: 'https://icdn.lenta.ru/images/2021/07/21/20/20210721200926212/square_320_3b7d8e82f70387b4bc934cb3da865b8f.jpg',
-                followed: false, fullName: 'John', status: 'I am a boss', location: { city: 'Michigan', country: 'USA' }
-            },
-            {
-                id: 3, photoURL: 'https://icdn.lenta.ru/images/2021/07/21/20/20210721200926212/square_320_3b7d8e82f70387b4bc934cb3da865b8f.jpg',
-                followed: false, fullName: 'Drew', status: 'I like tennis!', location: { city: 'Minsk', country: 'Belarus' }
-            },
-            {
-                id: 4, photoURL: 'https://icdn.lenta.ru/images/2021/07/21/20/20210721200926212/square_320_3b7d8e82f70387b4bc934cb3da865b8f.jpg',
-                followed: true, fullName: 'Pike', status: 'I love swim', location: { city: 'Kiev', country: 'Ukraine' }
-            }
-        ])
+    constructor(props) {
+        super(props);
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
     }
 
-    return (
-        <div>
-            {
-                props.users.map(u => <div key={u.id}>
-                    <span>
-                        <div>
-                            <img src={u.photoURL} alt="Avatar" className={styles.userPhoto} />
-                        </div>
-                        <div>
-                            {u.followed
-                                ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
-                        </div>
-                    </span>
-                    <span>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => <div key={u.id}>
                         <span>
-                            <div>{u.fullName}</div>
-                            <div>{u.status}</div>
+                            <div>
+                                <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="Avatar" className={styles.userPhoto} />
+                            </div>
+                            <div>
+                                {u.followed
+                                    ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                                    : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+                            </div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <span>
+                                <div>{u.name}</div>
+                                <div>{u.status}</div>
+                            </span>
+                            <span>
+                                <div>"u.location.country"</div>
+                                <div>"u.location.city"</div>
+                            </span>
                         </span>
-                    </span>
-                </div >)
-            }
-        </div >
-    );
+                    </div >)
+                }
+            </div >
+        );
+    }
 }
 
 export default Users;
