189707e9e5a033cabb9fbb73aa946c9420feff62
Pavel <bm38kpk@gmail.com>
2022-04-28 14:15:32 +0800

    Updates

---
+++ precache-manifest.56fe88b654bb1ce531a488b79433136e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e61ba5d451b3c69a988",
+    "url": "/Mars-social/static/css/main.2ad5f06b.chunk.css"
+  },
+  {
+    "revision": "9e61ba5d451b3c69a988",
+    "url": "/Mars-social/static/js/main.9e61ba5d.chunk.js"
+  },
+  {
+    "revision": "86a5ef37597f75e43af2",
+    "url": "/Mars-social/static/css/1.2489f2d4.chunk.css"
+  },
+  {
+    "revision": "86a5ef37597f75e43af2",
+    "url": "/Mars-social/static/js/1.86a5ef37.chunk.js"
+  },
+  {
+    "revision": "e9117fc84318b7f9c1f8",
+    "url": "/Mars-social/static/css/2.9d3aef88.chunk.css"
+  },
+  {
+    "revision": "e9117fc84318b7f9c1f8",
+    "url": "/Mars-social/static/js/2.e9117fc8.chunk.js"
+  },
+  {
+    "revision": "39033f8d92d353f98653",
+    "url": "/Mars-social/static/js/3.39033f8d.chunk.js"
+  },
+  {
+    "revision": "c7769136f3a1a931020b",
+    "url": "/Mars-social/static/js/runtime~main.c7769136.js"
+  },
+  {
+    "revision": "cac6f4f6ddbe92403ef75aab346d1f59",
+    "url": "/Mars-social/static/media/preloader.cac6f4f6.gif"
+  },
+  {
+    "revision": "2e195f2fedb8fe338fae08684c908863",
+    "url": "/Mars-social/static/media/logo.2e195f2f.jpg"
+  },
+  {
+    "revision": "a98513c4058a683189370a99a83d6e60",
+    "url": "/Mars-social/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Mars-social/precache-manifest.56fe88b654bb1ce531a488b79433136e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Mars-social/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
